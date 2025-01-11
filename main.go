86c81d07298e8eee8e73689857dbb14c86ad6d9b
Artur Aitmuratov <arturaitmuratov@gmail.com>
2025-01-12 01:04:41 +0500

    rename method

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ func main() {
 
 	nums1 := []int{1, 2}
 	nums2 := []int{3, 4}
-	getMedian(nums1, nums2)
+	printMedian(nums1, nums2)
 }
 
-func getMedian(nums1 []int, nums2 []int) {
+func printMedian(nums1 []int, nums2 []int) {
 	nums1 = append(nums1, nums2...)
 	slices.Sort(nums1)
 	if len(nums1)%2 == 0 {
