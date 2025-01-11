5ad7b35b97436a267de534211036a91043c58027
Artur Aitmuratov <arturaitmuratov@gmail.com>
2025-01-12 01:08:21 +0500

    change array

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	nums1 := []int{1, 2}
+	nums1 := []int{1, 2, 5, 6}
 	nums2 := []int{3, 4}
 	printMedian(nums1, nums2)
 }
@@ -16,8 +16,8 @@ func printMedian(nums1 []int, nums2 []int) {
 	nums1 = append(nums1, nums2...)
 	slices.Sort(nums1)
 	if len(nums1)%2 == 0 {
-		fmt.Println((nums1[(len(nums1)/2)-1] + nums1[len(nums1)/2]) / 2)
+		fmt.Println(float64(nums1[(len(nums1)/2)-1]+nums1[len(nums1)/2]) / 2.0)
 	} else {
-		fmt.Println(nums1[(len(nums1)-1)/2])
+		fmt.Println(float64(nums1[(len(nums1)-1)/2]))
 	}
 }
