71d59ade2c0bd198aedb0a40e59b6a76904e9afb
Artur Aitmuratov <arturaitmuratov@gmail.com>
2025-01-12 01:03:55 +0500

    rename

--- main.go
+++ main.go
@@ -7,17 +7,17 @@ import (
 
 func main() {
 
-	num1 := []int{1, 2}
-	num2 := []int{3, 4}
-	getMedian(num1, num2)
+	nums1 := []int{1, 2}
+	nums2 := []int{3, 4}
+	getMedian(nums1, nums2)
 }
 
-func getMedian(num1 []int, num2 []int) {
-	num1 = append(num1, num2...)
-	slices.Sort(num1)
-	if len(num1)%2 == 0 {
-		fmt.Println((num1[(len(num1)/2)-1] + num1[len(num1)/2]) / 2)
+func getMedian(nums1 []int, nums2 []int) {
+	nums1 = append(nums1, nums2...)
+	slices.Sort(nums1)
+	if len(nums1)%2 == 0 {
+		fmt.Println((nums1[(len(nums1)/2)-1] + nums1[len(nums1)/2]) / 2)
 	} else {
-		fmt.Println(num1[(len(num1)-1)/2])
+		fmt.Println(nums1[(len(nums1)-1)/2])
 	}
 }
